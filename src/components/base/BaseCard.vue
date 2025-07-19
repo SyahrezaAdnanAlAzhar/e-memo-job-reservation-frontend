5e04d2b37e3c70ac1fca28c3659ecd66f7a004f6
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-07-19 15:26:58 +0700

    feat: add base card

---
+++ src/components/base/BaseCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface Props {
+    customClass?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    customClass: '',
+});
+</script>
+
+<template>
+    <div 
+        class="
+        bg-mono-white 
+        rounded-xl 
+        shadow-md 
+        overflow-hidden
+        "
+        :class="props.customClass"
+    >
+        <div v-if="$slots.header" class="p-4 sm:p-6 border-b border-mono-lightgrey">
+            <slot name="header" />
+        </div>
+
+        <div class="p-4 sm:p-6">
+        <slot />
+        </div>
+
+        <div v-if="$slots.footer" class="p-4 sm:p-6 bg-mono-lightgrey bg-opacity-50">
+        <slot name="footer" />
+        </div>
+    </div>
+</template>
